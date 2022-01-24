bfeb0e94cdd9a6d482fd1fb2aa2510c09ef3efda
aLANparty <aLANparty@protonmail.com>
2022-01-23 21:51:42 -0500

    fix faucet url

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
                         context="snapshot"
                         contractAddr="0x81591DC4997A76A870c13D383F8491B288E09344"
                         deepLinkPrefix="brightid://link-verification/http:%2f%2fnode.brightid.org"
-                        faucetClaimURL="https://idchain.one/api/claim"
+                        faucetClaimURL="https://idchain.one/begin/api/claim"
                         mainnetRpcUrl="https://mainnet.infura.io/v3/fa20524651b3467098dbdca487a2e765"
                         registrationBlockExplorerUrl="https://explorer.idchain.one"
                         registrationBlockExplorerTxnPath="/tx/"
